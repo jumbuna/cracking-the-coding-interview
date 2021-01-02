c7f0426ddeaf34acaa5e592c174048af490d6494
jumbuna <jacobwangoni@yahoo.com>
2021-01-02 13:39:01 +0300

    initial solutions for Q1.3

---
+++ Solutions/Array_String/1_3/solution.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * Brute force solution
+ *
+ * Time Complexity:     O(n^3)
+ * Space Complexity:    O(1)
+ * n = length of string
+ */
+void removeDuplicateCharacters_Brute(char *str) {
+    size_t len = strlen(str);
+    for(int i = 0; i < len; i++) {
+        char c = str[i];
+        for(int j = i+1; j < len; j++) {
+            if(str[j] == c) {
+                for(int k = j; k < (len-1); k++) {
+                    str[k] = str[k+1];
+                }
+                str[--len] = 0;
+            }
+        }
+    }
+}
+
+/**
+ *
+ * Brute Force solution
+ *
+ * Time Complexity:     O(n^2)
+ * Space Complexity:    O(1)
+ * n = length of string
+ */
+ void removeDuplicateCharacters_Brute2(char *str) {
+     size_t len = strlen(str);
+     /* minimum length of the string after duplicate removal*/
+     int tail = 1;
+     for(int i = 0; i < len; i++) {
+         int j = 0;
+         /* check if str[i] is duplicate of a character in str[0:tail] */
+         for(; j < tail; j++) {
+             /* break once duplicate is found */
+             if(str[j] == str[i]) break;
+         }
+         /* if no duplicate was found the minimum length after removal 
+          * increases by one and str[i] becomes last character in the
+          * string.
+          *
+          * if str[i] is a duplicate found then tail remains the same and str[i]
+          * will be replaced with the next non duplicate found.
+         */
+         if(j == tail) {
+             str[tail++] = str[i];
+         }
+     }
+     /* make str[tail] the end of the string */
+     str[tail] = 0;
+ }
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        printf("Usage: %s str1 str2 ...\n", argv[0]);
+        return 1;
+    }
+    for(int i = 1; i < argc; i++) {
+        char *t = strdup(argv[i]);
+        removeDuplicateCharacters_Brute2(argv[i]);
+        printf("%s: %s\n", t, argv[i] );
+    }
+}
